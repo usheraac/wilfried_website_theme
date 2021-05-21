dcd29682d4fbf28398d420bec1c1d843f420eb9e
usheraac <djokoisaac@yahoo.fr>
2021-05-21 19:35:14 +0200

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": 'Wilfried Theme',
+    "summary": """
+        Apply wilfried design rules.
+    """,
+    "description": "Wilfried Magazine Theme ",
+    "author": "Isaac DJOKO",
+    "website": "",
+    "license": "LGPL-3",
+    "category": "Themes",
+    "version": "9.0",
+    "depends": ["website"],
+    "data": ["views/assets.xml"],
+}
