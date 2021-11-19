58813dcbdb7fcb948f54f762f2ea576c6ee448a6
coccinouille <victor@coopiteasy.be>
2021-11-19 11:41:51 +0100

    [ADD] wilfried christmas theme

---
+++ wilfried_website_theme_christmas/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": 'Wilfried Christmas Theme',
+    "summary": """
+        Apply wilfried design rules for christmas.
+    """,
+    "description": "Wilfried Magazine Christmas Theme ",
+    "author": "Isaac DJOKO",
+    "website": "",
+    "license": "LGPL-3",
+    "category": "Themes",
+    "version": "9.0.1.0.0",
+    "depends": ["website"],
+    "data": ["views/assets.xml"],
+}
